ec8a21391e8a55694ed919b0ceb1575f85c3b43a
adwaern <adamwaern@Adams-Air.lan>
2020-07-30 16:06:57 +0200

    Updates

---
+++ precache-manifest.672e4ee7b1cb1a77fd9af42d7f65a24d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b58cfc9ca1edfce463c",
+    "url": "/ravenous/static/css/main.4af94c7c.chunk.css"
+  },
+  {
+    "revision": "0b58cfc9ca1edfce463c",
+    "url": "/ravenous/static/js/main.c28c090b.chunk.js"
+  },
+  {
+    "revision": "1444c97d4f0ed3607f27",
+    "url": "/ravenous/static/js/runtime~main.f8ce170b.js"
+  },
+  {
+    "revision": "864f481808cf93127362",
+    "url": "/ravenous/static/js/2.10239577.chunk.js"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "7c7366912c18b651673f73168fde48bb",
+    "url": "/ravenous/index.html"
+  }
+];
